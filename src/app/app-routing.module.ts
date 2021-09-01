267084b4de81e73843d98ff810fcb236ec2594ce
Elton <elton@ingenie.com>
2021-09-01 23:27:11 +0100

    chore: grid started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-// import { ProjectsComponent } from './pages/projects/projects.component';
-// import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  // { path: 'projects', component: ProjectsComponent },
-  // { path: 'about-us', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'about-us', component: AboutComponent },
+  { path: 'testimonials', component: TestimonialsComponent },
   {
     path: 'contact-us',
     loadChildren: () =>
