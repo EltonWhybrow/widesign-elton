5c3195ab53b6d577fd6d43770bcbdecbad79351d
Elton <elton@ingenie.com>
2021-09-01 21:14:01 +0100

    chore: ready to start 🤞

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,50 @@
-const { colors } = require("tailwindcss/defaultTheme");
-
+const colors = require('tailwindcss/colors')
+// tailwind.config.js
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.ts"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    fontSize: {
-      base: "1rem",
-    },
-    extend: {
-      colors: {
-        primary: colors.teal,
+    colors: {
+      'blue-chill': {
+        '50': '#f3f9f9',
+        '100': '#e7f3f4',
+        '200': '#c3e1e2',
+        '300': '#9ecfd1',
+        '400': '#56acaf',
+        '500': '#0d888c',
+        '600': '#0c7a7e',
+        '700': '#0a6669',
+        '800': '#085254',
+        '900': '#064345'
       },
-    },
+      'korma': {
+        '50': '#faf6f3',
+        '100': '#f5ede8',
+        '200': '#e5d1c5',
+        '300': '#d5b5a1',
+        '400': '#b67e5b',
+        '500': '#964715',
+        '600': '#874013',
+        '700': '#713510',
+        '800': '#5a2b0d',
+        '900': '#4a230a'
+      },
+      'raw-sienna': {
+        '50': '#fef9f6',
+        '100': '#fcf3ed',
+        '200': '#f8e1d3',
+        '300': '#f4cfb8',
+        '400': '#ebab82',
+        '500': '#e3874d',
+        '600': '#cc7a45',
+        '700': '#aa653a',
+        '800': '#88512e',
+        '900': '#6f4226'
+      }
+    }
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+}
