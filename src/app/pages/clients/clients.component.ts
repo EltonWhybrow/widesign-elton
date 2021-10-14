a1bbf27cc3f5c606f69fe7c0351378d4fe2d3745
Elton <elton@ingenie.com>
2021-10-14 19:29:42 +0100

    chore: design change

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsComponent implements OnInit {
 
+  currentTab = 'generic';
+
   constructor() { }
+
+  handleEventClicked(data): void {
+    this.currentTab = data;
+  }
 
   ngOnInit(): void {
   }
